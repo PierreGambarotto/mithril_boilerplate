885d72bb0853d809c91722d87d844ebfa5454a46
Pierre Gambarotto <pierre.gambarotto@n7.fr>
2015-11-03 12:03:22 +0100

    hot module replacement with ud

--- client/app.js
+++ client/app.js
@@ -3,8 +3,12 @@ var Counter = require('./components/Counter');
 var Home = require('./components/Home');
 let inspect = require('util').inspect;
 require('./index.html');
+let ud = require('ud') // Hot Module Replacement
 
 m.route.mode = 'hash';
+
+// module is an object defined by webpack listing all the modules
+
 
 var model = {
   page: {
@@ -23,10 +27,12 @@ var model = {
   }
 };
 
-(function(){
+ud.defn(module, () => {
   return m.route(document.body, '/', {
     '/': m.component(Home, model.page) ,
     '/counter': m.component(Counter, model.counter)
   })
-}())
+})()
+                 
+
 
