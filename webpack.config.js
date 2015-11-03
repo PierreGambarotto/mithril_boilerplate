2b5c9fff45c60403bd035c44e79a9b1bcd85deb7
Pierre Gambarotto <pierre.gambarotto@n7.fr>
2015-11-03 09:27:53 +0100

    webpack using babel to handle msx in es6

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+// use variable in package.json
+var pkg = require('./package.json');
+var util = require('util');
+
+module.exports = {
+  context: path.join(__dirname, 'client', 'js'),
+  entry: {
+    app: ['./app.js']
+  },
+  devtool: 'source-map',
+  output: {
+      path: path.join(__dirname, pkg.config.buildDir),
+    publicPath: '/',
+    filename: 'js/[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'file?name=[path][name].[ext]'
+      },
+      {
+        test: /\.jpe?g$|\.svg$|\.png$/,
+        exclude: /node_modules/,
+        loader: 'file?name=[path][name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+  ]
+};
